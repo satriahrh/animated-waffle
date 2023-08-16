0e5d667c6f4910f6e1320a54f75076fc59e201ea
Satria H R Harsono <satriahrh@satriahrh.com>
2023-08-16 23:09:43 +0700

    sanitizer finished

--- sanitizer/fetch.go
+++ sanitizer/fetch.go
@@ -1,5 +1,21 @@
 package sanitizer
 
-func SanitizeFetch(args []string) error {
+import (
+	"fmt"
+	"regexp"
+)
 
+var urlRegex = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
+
+func SanitizeFetch(urls []string) error {
+	if len(urls) == 0 {
+		return fmt.Errorf("no url given")
+	}
+
+	for _, url := range urls {
+		if !urlRegex.MatchString(url) {
+			return fmt.Errorf("invalid url")
+		}
+	}
+	return nil
 }
--- sanitizer/fetch_test.go
+++ sanitizer/fetch_test.go
@@ -1,6 +1,7 @@
 package sanitizer_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/satriahrh/autify-tht/sanitizer"
@@ -12,7 +13,15 @@ func TestSanitizeFetch(t *testing.T) {
 		Name     string
 		Input    []string
 		Expected error
-	}{} {
+	}{
+		{"Empty", []string{}, fmt.Errorf("no url given")},
+		{"OneInvalid", []string{"not-a-file"}, fmt.Errorf("invalid url")},
+		{"OneInvalidHostOnly", []string{"google.com"}, fmt.Errorf("invalid url")},
+		{"OneSuccessSample", []string{"https://google.com"}, nil},
+		{"OneSuccessWithPath", []string{"https://google.com/webpage"}, nil},
+		{"MultipleHaveInvalid", []string{"https://google.com/", "invalid"}, fmt.Errorf("invalid url")},
+		{"MultipleSuccess", []string{"https://google.com/", "https://facebook.com"}, nil},
+	} {
 		t.Run(row.Name, func(t *testing.T) {
 			actual := sanitizer.SanitizeFetch(row.Input)
 			require.Equal(t, row.Expected, actual)
